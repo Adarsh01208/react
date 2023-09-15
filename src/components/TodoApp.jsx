b26cd8f8a19713dbb5b28e4035dfbebd28c7a266
Adarsh01208 <adarshsingh01209@gmail.com>
2023-09-15 18:00:30 +0530

    ''

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -17,16 +17,16 @@ const TodoApp = () => {
             console.log(newitems)
             setactivity("")
             return newitems
-
+          
         })
     }
 
     const deletetask = (index) => {
-        const temp = items
+        const temp =items 
         temp.splice(index, 1)
         setitems([...temp])
     }
-
+       
 
 
     return (
@@ -34,29 +34,25 @@ const TodoApp = () => {
             <div className="container mt-3">
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
-                        <h1 className="text-center">Todo App</h1>
-                        <div className="card shadow-lg">
-                            <div className="card-header">
+                        <div className="card  shadow-lg">
+                            <div className="card-header bg-warning text-success  ">
+                                <h1 className="text-center text-success ">Todo App</h1>
+                            </div>
+                            <div className="card-body  ">
                                 <div className="input-group mb-3">
                                     <input type="text" value={activity} className="form-control" onChange={inputtask} placeholder="Enter Item" />
                                     <button onClick={additem} className="btn btn-primary">Add</button>
                                 </div>
-                            </div>
-                            <div className="card-body ">
-                                {
-                                    items.length > 0 && items.map((item, index) => {
-                                        return (
-                                            // <div className="input-group shadow-lg mb-3" key={index}>
-                                            //     <input type="text" value={item} className="form-control" placeholder="Enter Tasks" />
-                                            //     <button onClick={deletetask} className="btn btn-danger">Delete</button>
-                                            // </div>
-                                            <ul className="list-group shadow-lg mb-3" key={index}>
-                                                <li className="list-group-item">{item}
-                                                    <button onClick={() => deletetask(index)} className="btn btn-danger float-end">Delete</button>
-                                                </li>
-                                            </ul>
-                                        )
-                                    })
+                                <br />
+                                <h3> Tasks</h3>
+                                {items.length > 0 && items.map((item, index) => {
+                                    return (
+                                        <div className="input-group shadow-lg mb-3" key={index}>
+                                            <input type="text" value={item} className="form-control" placeholder="Enter Tasks" />
+                                            <button onClick={deletetask} className="btn btn-danger">Delete</button>
+                                        </div>
+                                    )
+                                })
                                 }
                             </div>
                         </div>
